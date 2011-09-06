98a98eea38a9f759cad8745ffb97ac519104cda5
Simon Wade <simon.wade@gmail.com>
2011-09-06 12:27:44 +1000

    Minor improvement to RestrictedFileController. Added utils.js.

---
+++ javascript/utils.js
@@ -0,0 +1,15 @@
+$.ssTools = {
+	/**
+	 * Workaround for the fact that SS replaces all hash links to work around its base tag.
+	 * @param selector Optional selector for selecting links. Defaults to "a.hash-only, .hash-only a"
+	 */
+	fixHashLinks: function( selector ) {
+		$(selector || 'a.hash-only, .hash-only a').each(function(key, value) {
+			// handle the href with or without a leading slash at the start
+			var href = $(this).attr('href').replace(/^\//, ''),
+				location = window.location.pathname.replace(/^\//, '');
+			href = href.replace(location, '').replace(window.location.search, '');
+			$(this).attr('href', href);
+		});
+	}
+};
