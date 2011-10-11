835d0c81f5bce2bf6ae5db8d41c4a57c302ecab4
Simon Wade <simon.wade@gmail.com>
2011-10-11 23:09:15 +1100

    Added crawl-site(s) action to SSToolsController for easier pre-deployment testing.

--- code/SSToolsController.php
+++ code/SSToolsController.php
@@ -16,6 +16,34 @@ class SSToolsController extends CliController {
 		}
 	}
 
+	public function crawl_site( SS_HTTPRequest $request, $url = null ) {
+		$baseFolder = Director::baseFolder();
+		if( !$url ) {
+			$url = $request->getVar('url');
+		}
+		if( !$url ) {
+			$url = $GLOBALS['_FILE_TO_URL_MAPPING'][$baseFolder];
+		}
+		if( $url ) {
+			$outputFile = 'wget-output.log';
+			$command = "wget -o $outputFile -e robots=off -r -p '$url'";
+			echo "Crawling site '$url' using '$command'\n";
+			// If you're impatient, you can run:
+			// tail -f public/sapphire/wget-output.log | egrep 'awaiting response...' | egrep -v '(200 OK|302 Found)'
+			flush();
+			`$command`;
+			`cat $outputFile | egrep 'awaiting response...' | egrep -v '(200 OK|302 Found)'`;
+		}
+	}
+
+	public function crawl_sites( SS_HTTPRequest $request ) {
+		if( class_exists('Subsite') ) {
+			foreach( DataObject::get('Subsite', 'IsPublic = 1') as $subsite ) { /* @var $subsite Subsite */
+				self::crawl_site($request, $subsite->absoluteBaseURL());
+			}
+		}
+	}
+
 }
 
 ?>
