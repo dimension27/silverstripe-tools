be041375ea17e3898e65fc1f095ba16c1b9804f9
Sergei Matheson <sergei.matheson@gmail.com>
2011-10-19 11:54:30 +1100

    Added SSTools_StyledEmail

---
+++ code/StyledEmail.php
@@ -0,0 +1,28 @@
+<?php
+class SSTools_StyledEmail extends Email {
+	
+	public $CSS = null;
+	
+	public function send() {
+		$this->parseVariables();
+		$this->emogrifyBody();
+		return parent::send();
+	}
+
+	/**
+	 * Emogrify the body, if there's any CSS set for the email.
+	 * @return void
+	 */
+	protected function emogrifyBody() {
+			/* debug */ Debug::message('CSS'.$this->CSS);
+		if ($this->CSS) {
+			/* debug */ Debug::message('emogrifying');
+			$emogrifier = new Emogrifier();
+			$html = $this->body;
+			$emogrifier->setHTML($html);
+			$emogrifier->setCSS($this->CSS);
+			$this->body = $emogrifier->emogrify();
+			/* debug */ Debug::message($this->body);
+		}
+	}
+}
