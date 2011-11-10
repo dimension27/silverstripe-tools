b742555995a25a5a81438d63b88283d25c6ff22b
Simon Wade <simon.wade@gmail.com>
2011-11-11 09:43:11 +1100

    Bugfix

--- code/forms/CalendarField.php
+++ code/forms/CalendarField.php
@@ -4,7 +4,7 @@ class CalendarField extends DateField {
 
 	function __construct( $name, $title = null, $value = null, $form = null, $rightTitle = null ) {
 		parent::__construct($name, $title, $value, $form, $rightTitle);
-		$field->setConfig('showcalendar', true);
+		$this->setConfig('showcalendar', true);
 	}
 
 }
