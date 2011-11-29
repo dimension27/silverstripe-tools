202d8746fba4949c3025ab8f56ea4aaaccc89b9f
Adam Rice <development@madamimadam.org>
2011-11-29 17:40:35 +1100

    BetterImage: add better versions of getTag and getDimensions

--- code/BetterImage.php
+++ code/BetterImage.php
@@ -5,6 +5,47 @@
  */
 class BetterImage extends Image
 {   
+	
+	/**
+	 * Image version of getTag does not add width and height
+	 * 
+	 * @return string
+	 */
+	function getTag() {
+		return preg_replace('|/>$|', $this->getDimensions('tag') . ' />', parent::getTag());
+	}
+	
+	/**
+	 * Image version of getDimensions can return a string but it is pretty useless
+	 * The string returned by this version is ready for use in an img tag
+	 * @param string $dim	If this is equal to "tag", return the dimensions in a string formatted for an img tag
+	 *						If equal to "string", return "{height}x{width}
+	 *						If equal to 0 or "width", return width as an integer
+	 *						If equal to 1 or "height", return height as an integer
+	 * @return string|int
+	 */
+	function getDimensions($dim = "string") {
+		if($this->getField('Filename')) {
+			$imagefile = Director::baseFolder() . '/' . $this->getField('Filename');
+			if(file_exists($imagefile)) {
+				$size = getimagesize($imagefile);
+				if( $dim === 0 || $dim == 'width' )
+					$rv = $size[0];
+				else if( $dim === 1 || $dim == 'height' )
+					$rv = $size[1];
+				else if( $dim == 'tag' )
+					$rv = "height=\"{$size[1]}\" width=\"{$size[0]}\"";
+				else if( $dim === 'string' )
+					$rv = "$size[0]x$size[1]";
+				else
+					$rv = 'invalid value for $dim';
+				return $rv;
+			} else {
+				return ($dim === "string") ? "file '$imagefile' not found" : null;
+			}
+		}
+	}
+	
 	public function SetWidth($width) {
 		if($width == $this->getWidth()){
 			return $this;
@@ -39,7 +80,6 @@ class BetterImage extends Image
 
 		if( $height > $fullHeight && $width > $fullWidth )
 			return $this;
-		
 		$newHeight = ($width / $fullWidth) * $fullWidth;
 		return $newHeight < $height ? $this->SetWidth($width) : $this->SetHeight($height);
 	}
