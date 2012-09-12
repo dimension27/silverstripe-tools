04896d731e7f8ab586cb0c3766a391041abf2563
Simon Wade <simon.wade@gmail.com>
2012-09-12 17:47:25 +1000

    Added FormExample

---
+++ code/examples/FormExample.php
@@ -0,0 +1,42 @@
+<?php
+
+class FormExample extends ContentController {
+
+	function index() {
+		return $this->render(array(
+			'Form' => $this->DemoForm()
+		));
+	}
+
+	function DemoForm() {
+		$form = new DemoForm($this, 'DemoForm');
+		$form->Fields()->push(new TextField('Outside', 'Outside'));
+		return $form;
+	}
+
+}
+
+class DemoForm extends Form {
+
+	function __construct($controller, $name, FieldSet $actions, $validator = null) {
+		$fields = new FieldSet();
+		$fields->push(new TextField('Inside', 'Inside'));
+		$actions = new FieldSet();
+		$actions->push(new FormAction('doSubmit', 'Submit'));
+		$actions->push(new FormAction('doFoobar', 'Foobar'));
+		parent::__construct($controller, $name, $fields, $actions);
+	}
+
+	function doSubmit( $data ) {
+		echo 'doSubmit called'.NL;
+		/* debug */ Debug::show($data);
+	}
+
+	function doFoobar( $data ) {
+		echo 'doFoobar called'.NL;
+		/* debug */ Debug::show($data);
+	}
+
+}
+
+?>
