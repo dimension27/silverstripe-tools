e5a69771f5b1c1d3b60145e4226ae5d28772ec96
Simon Wade <simon.wade@gmail.com>
2012-10-03 14:59:26 +1000

    Added StringUtils;:createReplacements();

--- code/utils/StringUtils.php
+++ code/utils/StringUtils.php
@@ -2,6 +2,14 @@
 
 class StringUtils {
 
+	public static function createReplacements( $values ) {
+		$rv = array();
+		foreach( $values as $name => $value ) {
+			$rv["{\$$name}"] = $value;
+		}
+		return $rv;
+	}
+
 	function handleReplacements( $content, $replacements, $extra = array() ) {
 		if( $extra ) {
 			$replacements = array_merge($replacements, $extra);
